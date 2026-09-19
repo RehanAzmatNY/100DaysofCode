60fe8994bcda031e9c8968e5255bba1da21c6a2f
rehanazmatny <rehan.azmat.ny@gmail.com>
2026-09-19 00:33:48 -0400

    changed extension name

---
+++ S3D3 - Treasure Island.py
@@ -0,0 +1,15 @@
+#conditional if/else statements
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you are not tall enough to ride the rollercoaster.")
+
+#Comparison operators - > < >= <= == !=
+#Modulo operator - % (returns the remainder of a division)
+
+number_to_check = int(input("what is the number you want to check? "))
+
+print(number_to_check % 2) #returns 0 if even, 1 if odd
