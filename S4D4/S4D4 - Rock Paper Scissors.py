76154c0516d6a2f4b6154310ab0ef143fc860ef1
RehanAzmatNY <rehan.azmat.ny@gmail.com>
2026-09-22 17:30:47 -0400

    commit

--- S4D4/S4D4 - Rock Paper Scissors.py
+++ S4D4/S4D4 - Rock Paper Scissors.py
@@ -1,6 +1,6 @@
 import random                        #random module is responsible for generating random numbers                
-import My_Module 
+import My_Module
 
-random_integer = random.randint(1,10) #random numbers from 1-10
-print(random_integer)                #python module is responsible for a different piece of functionality
-print(My_Module.my_favorite_number)  #importing a module allows you to use the functions and variables defined in that module
+random_integer = random.randint(1,10)   #random numbers from 1-10
+print(random_integer)                   #python module is responsible for a different piece of functionality
+print(My_Module.my_favorite_number)     #importing a module allows you to use the functions and variables defined in that module
