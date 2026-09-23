d7af114042c1cb32cee5271e6ecebb773c87134c
RehanAzmatNY <rehan.azmat.ny@gmail.com>
2026-09-23 14:58:54 -0400

    Delete S4D4 - Rock Paper Scissors.py

--- S4D4 - Rock Paper Scissors.py
+++
@@ -1,6 +0,0 @@
-import random
-import my_module
-
-random_integer = my_module.randint(a=1, b=10) #random numbers from 1-10
-print(random_integer)     #python module is responsible for a different piece of functionality
-print(my_module.pi)  #accessing the value of pi from the my_module file
